6febf6d46948f1309fef49750c448fdb1a606a8f
Ahmad IBRAHIM <ahmadji.ibr11@gmail.com>
2023-05-20 22:36:59 +0200

    add function that open the video on youtube app when clicking on watch button

--- app/src/main/java/ahmad/io/youtubefavs/adapter/YouTubeVideoAdapter.java
+++ app/src/main/java/ahmad/io/youtubefavs/adapter/YouTubeVideoAdapter.java
@@ -1,9 +1,14 @@
 package ahmad.io.youtubefavs.adapter;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,13 +23,16 @@ public class YouTubeVideoAdapter extends RecyclerView.Adapter<YouTubeVideoAdapte
     private final List<YouTubeVideo> youTubeVideos;
 
     public static class YouTubeVideoViewHolder extends RecyclerView.ViewHolder {
-        public TextView tvTitle, tvDescription;
+        public TextView tvTitle, tvDescription, tvUrl;
+        public Button btnWatch;
 
         public YouTubeVideoViewHolder(@NonNull View itemView) {
             super(itemView);
 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvDescription = itemView.findViewById(R.id.tvDescription);
+            tvUrl = itemView.findViewById(R.id.tvUrl);
+            btnWatch = itemView.findViewById(R.id.btnWatch);
         }
     }
 
@@ -50,6 +58,23 @@ public class YouTubeVideoAdapter extends RecyclerView.Adapter<YouTubeVideoAdapte
         // Set the YouTube video information in the item view
         holder.tvTitle.setText(youTubeVideo.getTitle());
         holder.tvDescription.setText(youTubeVideo.getDescription());
+        holder.tvUrl.setText(youTubeVideo.getUrl());
+        holder.btnWatch.setOnClickListener(v -> {
+
+            // Get the URL of the YouTube video
+            String url = youTubeVideo.getUrl();
+
+            // Create an intent to watch the YouTube video
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+            // Start the activity
+            try {
+                v.getContext().startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                // Display a toast message if the YouTube app is not installed
+                Toast.makeText(v.getContext(), "The YouTube app is not installed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
